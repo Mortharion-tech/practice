6db109b532478922116ed97a9c677b63421ae006
Mortharion-tech <bjoernmueller1992@yahoo.de>
2024-08-13 13:32:23 +0200

    How Good Are You Really? task

---
+++ HowGoodAreYou/script.js
@@ -0,0 +1,22 @@
+/* There was a test in your class and you passed it. Congratulations!
+
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return true if you're better, else false!
+
+Note:
+Your points are not included in the array of your class's points. Do not forget them when calculating the average score! */
+
+function betterThanAverage(classPoints, yourPoints) {
+    const totalPoints = classPoints.reduce((acc, pts) => acc + pts, 0) + yourPoints;
+    const averageScore = totalPoints / (classPoints.length + 1);
+    if (yourPoints > averageScore) {
+      return true;
+    } else if (yourPoints < averageScore) {
+      return false;
+    } else {
+      return "You are average!";
+    }
+  }
